559aaef976264c76aeb1f1eddd0c0c02f38fe216
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-05-23 18:24:04 +0545

    feat: implemented blur in sidebar

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -22,5 +22,295 @@ export const chatList = [
     },
     {
         title: "I love programming",
+    },
+
+    // 
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+
+    // 
+
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+
+    //
+
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+
+    // 
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+
+    // 
+
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+
+    //
+
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+
+    // 
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+
+    // 
+
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
+    },
+
+    //
+
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "System Check and Project Discussion",
+    },
+    {
+        title: "QA's are awesome",
+    },
+    {
+        title: "Devs are fantastic",
+    },
+    {
+        title: "React.js is a library",
+    },
+    {
+        title: "Redux has a centralized state store test test test test test ",
+    },
+    {
+        title: "I love programming",
     }
+
+
 ]
